fcf7e5b8169a32918bd292f121e96d2a1baee5ce
Mrudula A P <122630243+mrudulamadhavan@users.noreply.github.com>
2025-07-02 00:02:43 +0530

    Update algebra_assistant_app.py

--- algebra_assistant_app.py
+++ algebra_assistant_app.py
@@ -12,8 +12,8 @@ st.set_page_config(page_title="Algebra Assistant Bot", page_icon="📘")
 st.title("📘 Algebraic Assistant Chatbot")
 
 st.markdown("🔍 **Enter an algebraic expression or equation:**")
-st.write("NB:  Use ** for powers and * for multiplication.")
-query = st.text_input("e.g., x**2 + 3*x = 10 or (x+2)*(x-3)")
+st.write("ℹ️ Use `**` for powers and `*` for multiplication.")
+query = st.text_input("", placeholder="e.g., x**2 + 3*x = 10 or (x+2)*(x-3)")*(x-3)")
 
 riddles_and_answers = [
     ("I’m always in the middle of algebra, but never in geometry. What am I?", "The letter 'b'"),
